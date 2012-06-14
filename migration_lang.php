f0aece6ab6684e3299db2177bf7141770fbe6016
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-06-14 11:56:44 +0800

    fix migration language for 2.1.1 release
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- migration_lang.php
+++ migration_lang.php
@@ -5,7 +5,7 @@ $lang['migration_not_found'] = "此 Migration 不存在";
 $lang['migration_multiple_version'] = "多重 migrations 擁有相同的版本號碼: %d";
 $lang['migration_class_doesnt_exist'] = "Migration 類別 \"%s\" 不存在";
 $lang['migration_missing_up_method'] = "Migration 類別 \"%s\" i缺少 'up' 方法";
-$lang['migration_missing_down_method'] = "Migration 類別 \"%s\" 缺少 'up' 方法";
+$lang['migration_missing_down_method'] = "Migration 類別 \"%s\" 缺少 'down' 方法";
 $lang['migration_invalid_filename'] = "Migration \"%s\" 包含無效的欄位名稱";
 
 
