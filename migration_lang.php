53d62b8dcccf64727e6afc6cf1b2cfa5bf44068f
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-12-21 14:51:00 +0800

    update some error.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- migration_lang.php
+++ migration_lang.php
@@ -38,7 +38,7 @@
  */
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['migration_none_found'] = '沒有發現遷移方法';
+$lang['migration_none_found'] = '沒有發現任何遷移';
 $lang['migration_not_found'] = '無法根據版本號碼 %s 找到遷移方法';
 $lang['migration_sequence_gap'] = '版本遷移存在間隙：%s';
 $lang['migration_multiple_version'] = '有多個遷移對應到同一版本號：%s';
